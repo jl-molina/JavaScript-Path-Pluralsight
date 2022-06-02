866f845a3591ef109825301f12cc1641ee5d77c0
jl-molina <joseluisml1218@gmail.com>
2022-06-02 09:02:23 -0600

    Simple switch statement

---
+++ Syntax and Operators/app.js
@@ -0,0 +1,15 @@
+function simpleSwitch() {
+    let productId = 3;
+
+    switch(productId) {
+        case 1:
+            console.log('Product 1');
+            break;
+        case 2:
+            console.log('Product 2');
+            break;
+        default:
+            console.log('Unknown product');
+            break;
+    }
+}
